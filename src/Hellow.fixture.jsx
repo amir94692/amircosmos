80793ed09705ae189914e08defc1143e303e675d
amirjs <amirooouuu@gmail.com>
2020-10-09 13:03:24 -0400

    save4

--- src/Hellow.fixture.jsx
+++ src/Hellow.fixture.jsx
@@ -2,8 +2,5 @@
 import React from 'react';
 import { Hello } from '../Hello';
 
-<<<<<<< HEAD
 export default <Hello greeting="Aloha" name="Alexa" amir="amir"  key="25"/>
-=======
-export default <Hello />
->>>>>>> amir
+
