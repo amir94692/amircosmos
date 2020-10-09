7e151d687ebd5f95c908a478d7a6d79822d3f7b9
amirjs <amirooouuu@gmail.com>
2020-10-09 12:23:56 -0400

    save stash chaange

--- src/Hellow.fixture.jsx
+++ src/Hellow.fixture.jsx
@@ -2,4 +2,8 @@
 import React from 'react';
 import { Hello } from '../Hello';
 
+<<<<<<< Updated upstream
 export default <Hello greeting="Aloha" name="Alexa" amir="amir" />
+=======
+export default <Hello greeting="Aloha" name="Alexa" amir="amir"  key="25"/>
+>>>>>>> Stashed changes
