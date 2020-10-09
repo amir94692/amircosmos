36c7b6a11157bd5d68bbe8d1407d50fba0ec5f21
amirjs <amirooouuu@gmail.com>
2020-10-09 13:44:20 -0400

    asas

--- src/Hellow.fixture.jsx
+++ src/Hellow.fixture.jsx
@@ -3,5 +3,5 @@ import React from 'react';
 import { Hello } from '../Hello';
 
 
-export default <Hello greeting="Aloha" name="Alexa" p={5} />
+export default <Hello greeting="Aloha" name="Alexa" amir="123456"/>
 
