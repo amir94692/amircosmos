a03fb4c6eb87836017cf1247a058019451a14600
amirjs <amirooouuu@gmail.com>
2020-10-09 12:32:30 -0400

    cosmos 4

--- src/Hellow.fixture.jsx
+++ src/Hellow.fixture.jsx
@@ -2,8 +2,4 @@
 import React from 'react';
 import { Hello } from '../Hello';
 
-<<<<<<< Updated upstream
-export default <Hello greeting="Aloha" name="Alexa" amir="amir" />
-=======
 export default <Hello greeting="Aloha" name="Alexa" amir="amir"  key="25"/>
->>>>>>> Stashed changes
